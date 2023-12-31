eae08c77e82ff878dd12c2f1da4c3dc1f5644090
lishuzeng <15989194692@163.com>
2023-12-31 19:49:14 +0800

    调整url路径

--- gateway/src/main/java/com/lsz/TestController.java
+++ gateway/src/main/java/com/lsz/TestController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @Version 1.0.0
  **/
 @RestController
-@RequestMapping("test")
+@RequestMapping("spike-web")
 public class TestController {
 
     @GetMapping("/test")
